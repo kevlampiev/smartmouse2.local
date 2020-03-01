803d6cfa7cb6fd8c40d0ab9c4181dd77dfb22fac
kevlampiev <kevlampiev@gmail.com>
2020-03-01 13:32:12 +0700

    added simfony loader

--- public/index.php
+++ public/index.php
@@ -14,9 +14,12 @@ use Symfony\Component\Routing\Loader\AnnotationDirectoryLoader;
 use Composer\Autoload\ClassLoader;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
+
+
 /** @var ClassLoader $loader */
 $loader = require __DIR__ . '/../vendor/autoload.php';
 
+AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 
 
 $loader = new AnnotationDirectoryLoader(
